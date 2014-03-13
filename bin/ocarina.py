cbec5912bfebf722f32790e03fb78231abc02169
Niklas Lindblad <niklas@lindblad.info>
2014-03-13 21:00:39 +0000

    Refactor chord runtime and add logging

--- bin/ocarina.py
+++ bin/ocarina.py
@@ -1,7 +1,31 @@
 #!/usr/bin/env python
 
+import argparse
+import logging
 import os
+import platform
 import sys
+
+root = logging.getLogger()
+ch = logging.StreamHandler( sys.stdout )
+formatter = logging.Formatter( '%(asctime)s - ocarina - %(levelname)s - %(message)s' )
+ch.setFormatter( formatter )
+root.addHandler( ch )
+
+# Parse command line arguments 
+args_parser = argparse.ArgumentParser()
+args_parser.add_argument( "--debug", help="Increase output verbosity", action="store_true" )
+args = args_parser.parse_args()
+
+ # Adjust logging to desired verbosity
+if args.debug:
+  ch.setLevel( logging.DEBUG )
+  root.setLevel( logging.DEBUG ) 
+else:
+  ch.setLevel( logging.INFO )
+  root.setLevel( logging.INFO )
+
+logging.debug( 'Using Python %s', platform.python_version() )
 
 # Add relevant directories to path
 currentDirectory = os.path.dirname( os.path.abspath( __file__ ) )
@@ -16,4 +40,4 @@ sys.path.insert( 0, chordsDirectory )
 import core.chords as chords
 from core.now import Now
 
-chords.run( chordsDirectory, Now() )
+chords.run( chordsDirectory, Now(), logging )
--- core/chords.py
+++ core/chords.py
@@ -2,28 +2,34 @@ import datetime
 import importlib
 import os
 
-def today():
-    now = datetime.datetime.today().timetuple()
-    print now
-
-def findChords(directory, now):
+def findChords(directory, logging):
+    chords = []
+    logging.debug( 'Looking for chords in %s', directory )
     for root, dirs, files in os.walk( directory ):
-        files = [ f for f in files if f.endswith( '.pyc' ) ]
+        files = [ f for f in files if not f.endswith( '.pyc' ) ]
         for file in files:
             package = os.path.basename( root )
             name = file.split( '.' )[0]
             module = "%s.%s" % ( package, name )
             if name != '__init__':
-                print module
-                chord = importlib.import_module( module )
-                shouldRun = chord.shouldRun( now )
-                print shouldRun
-                if shouldRun:
-                    try:
-                        chord.main()
-                    except:
-                        pass
+                logging.debug( 'Found chord %s', file )
+                chords.append( module )
+    return chords
 
+def runModule(chord, now, logging):
+    module = importlib.import_module( chord )
+    logging.debug( 'Considering whether to run %s', chord )
+    shouldRun = module.shouldRun( now )
+    logging.debug( 'shouldRun() returned %s', shouldRun )
+    if shouldRun:
+        try:
+            logging.debug( 'Running main method on %s', chord )
+            module.main()
+        except:
+            logging.error( 'Failed to run %s', chord )
+            pass
 
-def run(directory, now):
-    findChords( directory, now )
+def run(directory, now, logging):
+    chords = findChords( directory, logging )
+    for chord in chords:
+        runModule( chord, now, logging )
