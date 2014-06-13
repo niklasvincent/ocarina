96d77b57792350cde7a2d38748fe238d3221564d
Niklas Lindblad <niklas@lindblad.info>
2014-06-13 20:12:17 +0100

    Restructure main program #11

--- bin/ocarina.py
+++ bin/ocarina.py
@@ -7,60 +7,81 @@ import os
 import platform
 import sys
 
-# Parse command line arguments
-args_parser = argparse.ArgumentParser()
-args_parser.add_argument(
-    "--debug",
-    help="Increase output verbosity",
-    action="store_true")
-args_parser.add_argument("--tweak", help="Set time and date to tweaked value")
-args_parser.add_argument("--chords", help="Directory to look for scripts in")
-args = args_parser.parse_args()
+currentDirectory = None
 
-# Add relevant directories to path
-currentDirectory = os.path.dirname(os.path.abspath(__file__))
-for path in ['../core', '../lib', '..']:
-    path = os.path.abspath(os.path.join(currentDirectory, path))
-    sys.path.insert(0, path)
 
-# Load configuration
-from core.config import config as conf
+def setup_path():
+    """ Set up path """
+    global currentDirectory
+    currentDirectory = os.path.dirname(os.path.abspath(__file__))
+    for path in ['../core', '../lib', '..']:
+        path = os.path.abspath(os.path.join(currentDirectory, path))
+        sys.path.insert(0, path)
 
-# Set up logging
-import core.log as log
-logging = log.getLogger(args.debug)
 
-# Set up database
-import core.database
-try:
-    databaseFromConf = conf.get('ocarina', 'database')
-except:
-    databaseFromConf = None
-if databaseFromConf is None:
-    databaseFile = os.path.abspath(os.path.join(
-                                   currentDirectory, '../results.db'))
-else:
-    databaseFile = databaseFromConf
-db = core.database.getDatabase(databaseFile)
-
-logging.debug('Using Python %s', platform.python_version())
-
-# Where are the chords kept?
-if args.chords:
-    if os.path.isdir(args.chords):
-        chordsDirectory = args.chords
-else:
+def setup_db():
+    global currentDirectory
+    # Set up database
+    import core.database
     try:
-        chordsDirectoryFromConf = conf.get('ocarina', 'chords')
-        if os.path.isdir(chordsDirectoryFromConf):
-            chordsDirectory = chordsDirectoryFromConf
+        databaseFromConf = conf.get('ocarina', 'database')
     except:
-        chordsDirectory = os.path.abspath(
-            os.path.join(currentDirectory, '../chords'))
-sys.path.insert(0, chordsDirectory)
+        databaseFromConf = None
+    if databaseFromConf is None:
+        databaseFile = os.path.abspath(os.path.join(
+                                       currentDirectory, '../results.db'))
+    else:
+        databaseFile = databaseFromConf
+    db = core.database.getDatabase(databaseFile)
+    return db
 
-import core.chords as chords
-from core.now import Now
 
-now = Now(args.tweak)
-chords.run(chordsDirectory, now, logging)
+def main():
+    setup_path()
+
+    # Parse command line arguments
+    args_parser = argparse.ArgumentParser()
+    args_parser.add_argument(
+        "--debug",
+        help="Increase output verbosity",
+        action="store_true")
+    args_parser.add_argument("--tweak",
+                             help="Set time and date to tweaked value")
+    args_parser.add_argument("--chords",
+                             help="Directory to look for scripts in")
+    args = args_parser.parse_args()
+
+    # Set up logging
+    import core.log as log
+    logging = log.getLogger(args.debug)
+    logging.debug('Using Python %s', platform.python_version())
+
+    # Load configuration
+    from core.config import config as conf
+
+    # Set up database
+    setup_db()
+
+    # Where are the chords kept?
+    if args.chords:
+        if os.path.isdir(args.chords):
+            chordsDirectory = args.chords
+    else:
+        try:
+            chordsDirectoryFromConf = conf.get('ocarina', 'chords')
+            if os.path.isdir(chordsDirectoryFromConf):
+                chordsDirectory = chordsDirectoryFromConf
+        except:
+            chordsDirectory = os.path.abspath(
+                os.path.join(currentDirectory, '../chords'))
+    sys.path.insert(0, chordsDirectory)
+
+    import core.chords as chords
+    from core.now import Now
+
+    now = Now(args.tweak)
+    chords.run(chordsDirectory, now, logging)
+
+
+if __name__ == "__main__":
+    main()
