22d70569e9f4b15d5fe5eab825db7234076f5681
Niklas Lindblad <niklas@lindblad.info>
2015-06-06 16:21:14 +0100

    Make it possible for chords to store key/value pairs

--- core/database.py
+++ core/database.py
@@ -1,3 +1,4 @@
+import inspect
 import sys
 import sqlite3
 
@@ -6,25 +7,26 @@ logging = log.getLogger()
 
 from model import model
 
-databaseInstance = None
+databaseInstance = {}
 
 
-def getDatabase(filename='results.db'):
+def getInstance(filename='results.db', createModel = False):
     global databaseInstance
-    if databaseInstance is not None:
-        return databaseInstance
-    databaseInstance = Database(filename)
-    return databaseInstance
+    if databaseInstance.get(filename) is not None:
+        return databaseInstance.get(filename)
+    databaseInstance[filename]= Database(filename, createModel)
+    return databaseInstance[filename]
 
 
 class Database(object):
 
-    def __init__(self, filename):
+    def __init__(self, filename, createModel = False):
         try:
             logging.debug('Attempting to open database %s', filename)
             self.conn = sqlite3.connect(filename)
             self.cursor = self.conn.cursor()
-            self._setupNecessary()
+            if createModel:
+                self._setupNecessary()
         except Exception as e:
             logging.critical('Could not open database %s: %s', filename, e)
             sys.exit(3)
@@ -52,6 +54,28 @@ class Database(object):
         except Exception as e:
             logging.critical('Could not execute query: %s', e)
             sys.exit( 7 )
+
+    def _getChord(self):
+        """Get the module name of the chord making the database call"""
+        frm = inspect.stack()[3]
+        mod = inspect.getmodule(frm[0])
+        return mod.__name__
+
+    def _constructKeyName(self, key):
+        return '.'.join([self._getChord(), key])
+
+    def get(self, key):
+        sql = '''SELECT value FROM state WHERE key = ?'''
+        return self._executeQuery(sql, [self._constructKeyName(key)], return_result = True)
+
+    def set(self, key, value):
+        sql = '''INSERT OR REPLACE INTO state ("key", "value") VALUES (?, ?)'''
+        self._executeQuery(
+            sql,
+            [self._constructKeyName(key), value],
+            return_result = False,
+            commit = True
+        )
 
     def recordExecution(self, start_time, chord_name, execution_time, status,
                         output):
